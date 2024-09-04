6f067f50fe56415e7180c55ef30af3139d0d22a3
Alexladno <153184970+Alexladno@users.noreply.github.com>
2024-09-04 18:31:31 +0300

    old ver

---
+++ Версии расписания/17.02.2024 (фиолка)/DataBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Регистрация
+{
+    internal class DataBase
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=ADMIN\SQLEXPRESS;Initial Catalog=diplom;
+        Integrated Security=True");
+        
+        public DataBase() { }
+
+        public void openConnection()
+        {
+           if(sqlConnection.State == System.Data.ConnectionState.Closed) 
+            {
+            sqlConnection.Open();
+            }
+        }
+
+        public void  closedConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return sqlConnection;
+        }
+
+        internal User AuthenticateUserAsync(string login, string pass)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Версии расписания/17.02.2024 (фиолка)/Login.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.SqlServer.Server;
+
+namespace Регистрация
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+
+
+        DataBase database = new DataBase();
+
+
+        public Login()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void ButtonVoiti(object sender, RoutedEventArgs e)
+        {
+            var loginUser = textBoxLogin.Text;
+            var passUser = passBox.Password;
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+
+
+            string querystring = $"select id, login, pass, email from users where login = '{loginUser}' and pass = '{passUser}'";
+
+            SqlCommand command = new SqlCommand(querystring, database.GetConnection());
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+            if (table.Rows.Count == 1)
+            {
+                MessageBox.Show("Вы успешно вошли!");
+                UserPageWindow userPageWindow = new UserPageWindow();
+                userPageWindow.Show();
+                Hide();
+            }
+            else
+            {
+                MessageBox.Show("ЛОХ", "ЛОХ!");
+            }
+            }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Hide();
+        }
+    }
+    }
+
+
+    
+    
+
